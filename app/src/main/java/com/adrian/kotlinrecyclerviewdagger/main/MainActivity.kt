e0c4d67e351a0e581142d14204b4de5848aa18be
Adrian Czigany <c.adrian89@gmail.com>
2017-07-25 23:28:22 +0200

    basic recyclerview

--- app/src/main/java/com/adrian/kotlinrecyclerviewdagger/main/MainActivity.kt
+++ app/src/main/java/com/adrian/kotlinrecyclerviewdagger/main/MainActivity.kt
@@ -3,8 +3,12 @@ package com.adrian.kotlinrecyclerviewdagger.main
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.adrian.kotlinrecyclerviewdagger.R
+import com.adrian.kotlinrecyclerviewdagger.domain.ListItem
 import com.adrian.kotlinrecyclerviewdagger.main.di.MainComponent
+import com.adrian.kotlinrecyclerviewdagger.recyclerview.MyAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +18,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
-
         MainComponent.Injector.buildComponent(this).inject(this);
 
-        sharedPreferences.toString()
+        list.layoutManager = LinearLayoutManager(this)
+        list.hasFixedSize()
+        list.adapter = MyAdapter(this, getLists())
+    }
+
+    fun getLists(): ArrayList<ListItem> {
+        val listItem1 = ListItem(1, "Kotlin", "www.kotlin.com")
+        val listItem2 = ListItem(2, "Java", "www.java.com")
+        val listItem3 = ListItem(3, "Android", "www.android.com")
+        val listItem4 = ListItem(4, "JavaScript", "www.javascript.com")
+        val lists = ArrayList<ListItem>()
+        lists.add(listItem1)
+        lists.add(listItem2)
+        lists.add(listItem3)
+        lists.add(listItem4)
+        return lists;
     }
 }
---
+++ app/src/main/java/com/adrian/kotlinrecyclerviewdagger/recyclerview/MyAdapter.kt
@@ -0,0 +1,39 @@
+package com.adrian.kotlinrecyclerviewdagger.recyclerview
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.adrian.kotlinrecyclerviewdagger.R
+import com.adrian.kotlinrecyclerviewdagger.domain.ListItem
+import kotlinx.android.synthetic.main.list_item_layout.view.*
+
+/**
+ * Created by cadri on 2017. 07. 25..
+ */
+
+class MyAdapter(var context: Context, var lists: ArrayList<ListItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder? {
+        var v = LayoutInflater.from(context).inflate(R.layout.list_item_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        (holder as ViewHolder).bindData(lists[position])
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindData(item: ListItem) {
+            itemView.itemId.text = item.itemId.toString()
+            itemView.title.text = item.title
+            itemView.url.text = item.url
+        }
+    }
+}
